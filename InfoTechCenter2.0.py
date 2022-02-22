8d6c972d108a0011dd38f918b505c0db10be4557
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-22 13:22:58 -0700

    Imported Time Library and used Sleep Function

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,6 +1,9 @@
+from time import sleep #Print to one line with time delay between prints
 # Code Name - Hornet
 
-print("Welcome to Hornets InfoTech Center\n")
-
+print("Welcome to Hornets InfoTech Center\n");sleep(1.5)
 print("Hornet's Operating System Booting Up")
+print("Initializing.")
+print("Initializing..")
+
 
