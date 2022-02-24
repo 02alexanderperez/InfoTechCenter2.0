3d225dc106df670ae5942af8310554c8673e4c4c
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-24 13:55:22 -0700

    Declared  vehicleResponseSystem Function -IF - Stabe

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -9,7 +9,13 @@ def weatherList():
 #Calling weather function for obvious reasons
 weatherAlert = weatherList()
 
+def VRS():
+     if weatherAlert == "Ice":
+         print("\nVRS has changed your alarm 30 minutes early based on NWS reports of",weatherAlert)
+         print("VRS will only allow your car to go 30 MPH")
 
+
+vehicleResponseSystem():
 
 
 
