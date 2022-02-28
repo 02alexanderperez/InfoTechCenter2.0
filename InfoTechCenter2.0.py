09a570bd99029508a387d915047b8b12e08be332
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-28 13:45:19 -0700

    VRS defined and stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -18,14 +18,16 @@ def VRS():
         print("VRS will only allow your car to go 50 MPH")
     elif weatherAlert == "Heavy Rain":
         print("\nVRS has changed your alarm 60 minutes early based on NWS reports of", weatherAlert)
-        print("VRS will only allow your car to go 30 MPH")
+        print("VRS will only allow your car to go 60 MPH")
     elif weatherAlert == "Windy":
         print("\nVRS has changed your alarm 30 minutes early based on NWS reports of", weatherAlert)
-        print("VRS will only allow your car to go 60 MPH")
+        print("VRS will only allow your car to go 70 MPH")
     elif weatherAlert == "Foggy":
         print("\nVRS has changed your alarm 20 minutes early based on NWS reports of", weatherAlert)
-        print("VRS will only allow your car to go 35 MPH")
-    elif weatherAlert == "Sunny":
+        print("VRS will only allow your car to go 60 MPH")
+    else:
         print("\nVRS has not changed your alarm based on NWS reports of", weatherAlert)
         print("VRS will allow your car to go up to 120 MPH")
-print(VRS())
+
+
+vehicleResponseSystem()
